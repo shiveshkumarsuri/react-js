bb404a1bdc38f9635679a67eed998e355b10bdbb
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-07-25 15:13:50 -0500

    click-event-example-3

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,16 +1,10 @@
 function Button(){
     let count = 0;
-    const handleClick = (name) =>{
-        if(count < 3){
-            count++;
-            console.log(`${name} clicked me ${count} time/s`);
-        }else{
-            console.log(`${name} Stop Clicking me Now!`);
-        }
+    const handleClick = (e) =>{
+        e.target.textContent="OUCH"
     };
-
     return(
-        <button onClick={() => handleClick("Shivesh")}> Click Me</button>
+        <button onClick={(e) => handleClick(e)}> Click Me</button>
     );
 }
 export default Button
