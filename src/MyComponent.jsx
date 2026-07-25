80628eb16a29d02bff95d98b7aacff4aafb332db
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-07-25 15:48:47 -0500

    useState- toggle flag

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -5,6 +5,7 @@ function MyComponent(){
     
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [employeed, setIsEmployeed] = useState(false);
 
     const updateName = () =>{
         setName("Shivesh");
@@ -13,12 +14,18 @@ function MyComponent(){
         setAge(age +1);
     }
 
+    const toogleIsEmployeed = () => {
+        setIsEmployeed(!employeed);
+    }
+
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
             <p>Age:{age}</p>
             <button onClick={incrementAge}>Increase Age</button>
+            <p>Is Employyed:{employeed ? "Yes" : "No"}</p>
+            <button onClick={toogleIsEmployeed}>Toggle Employement</button>
         </div>
     );
 }
