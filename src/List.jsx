211eff479321270d6a0d954bce6f98f6aa646635
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-07-25 02:34:36 -0500

    list-default and prop-type check

--- src/List.jsx
+++ src/List.jsx
@@ -1,31 +1,38 @@
+import PropTypes from "prop-types";
 
-function List(){
-    const fruits = [{id:1, name:"Apple", calories:95},
-                    {id:2, name: "Banana",calories:120},
-                    {id:3, name:"Mango", calories:200},
-                    {id:4, name:"Pineapple", calories:160},
-                    {id:5, name:"Oranges", calories:150}];
+function List(props){
+    
+    const {items = [], category = "Category"} = props;
+    const itemList = [...items];
 
     //fruits.sort((a,b)=> a.name.localeCompare(b.name)); //ALPHABETICAL
     //fruits.sort((a,b)=> b.name.localeCompare(a.name)); //REVERSE-ALPHABETICAL
-    fruits.sort((a,b)=> a.calories - b.calories); //NUMERIC SORT
+    itemList.sort((a,b)=> a.calories - b.calories); //NUMERIC SORT
     
-    const lowCalFruits = fruits.filter(fruit => 
+    const lowCalFruits = itemList.filter(fruit => 
                                         fruit.calories <= 150
                                     );
 
     const listItems = lowCalFruits.map(fruit =>   
                                         <li key={fruit.id}>
                                             {fruit.name}: &nbsp;
-                                            <b>{fruit.calories}</b>
+                                        <b>{fruit.calories}</b>
                                         </li>
                                 );
-
-    
-
-    return(
-        <ol>{listItems}</ol>
+    return(<>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+        </> 
     );
+}
+
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+                                id: PropTypes.number,
+                                name: PropTypes.string,
+                                calories: PropTypes.number
+                             })),
 }
 
 export default List
