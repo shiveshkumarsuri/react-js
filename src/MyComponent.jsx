b9b67b5fbc2639c94ba5e3d7b5e26d55289bbace
shivesh Kumar <shiveshkumarsuri@hotmail.com>
2026-07-25 15:43:55 -0500

    useState- increment counter

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,15 +4,21 @@ function MyComponent(){
 
     
     const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
 
     const updateName = () =>{
         setName("Shivesh");
+    }
+    const incrementAge = () =>{
+        setAge(age +1);
     }
 
     return(
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+            <p>Age:{age}</p>
+            <button onClick={incrementAge}>Increase Age</button>
         </div>
     );
 }
